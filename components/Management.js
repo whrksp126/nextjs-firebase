1dff549b3aefd889e310b953858ebf67ec71cc60
whrksp126 <whrksp126@naver.com>
2022-02-03 13:03:04 +0900

    oil change

--- components/Management.js
+++ components/Management.js
@@ -7,10 +7,6 @@ import { collection, doc, getDocs, query, setDoc } from 'firebase/firestore';
 
 import { useAuth } from '../Auth';
 
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DatePicker from '@mui/lab/DatePicker';
-
 const Management = () => {
 
   const {
@@ -19,8 +15,6 @@ const Management = () => {
 
 
   // 차량 등록 기능 구현
-  const [addCar, setAddCar] = useState(true);
-
   const [open, setOpen] = useState(false);
   const handleClickOpen = () => {
     setOpen(true);
@@ -31,169 +25,118 @@ const Management = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setCarNameError(false)
-    setCarNumberError(false)
-    if (carName === '') {
-      setCarNameError(true)
+    setOilChangePriceError(false)
+    setOilChangeDateError(false)
+    if (oilChangePrice === '') {
+      setOilChangePriceError(true)
     }
-    if (carNumber === '') {
-      setCarNumberError(true)
+    if (oilChangeDate === '') {
+      setOilChangeDateError(true)
     }
-    if (carName && carNumber) {
+    if (oilChangePrice && oilChangeDate) {
       try {
-        await setDoc(doc(db, 'carManagement', carNumber), {
-          carName: carName,
-          carNumber: carNumber,
-          carImage: carImage,
+        await setDoc(doc(db, 'carManagement', oilChangeDate), {
+          oilChangePrice: oilChangePrice,
+          oilChangeDate: oilChangeDate,
+          oilChangeName: oilChangeName,
+          oilChangeMemo: oilChangeMemo,
           userEmail: currentUser.email
         });
-        setCarNumber('');
+        setOilChangeDate('');
 
       } catch {
         console.log('오류 발생')
       }
-      setCarName('');
-      setCarNumber('');
-      setCarImage('');
+      setOilChangePrice('');
+      setOilChangeDate('');
+      setOilChangeName('');
+      setOilChangeMemo('');
       setOpen(false);
       location.reload()
     } else {
       console.log('모델 명과 차량 번호는 필수 사항 입니다.')
     }
-    // console.log('carName은', carName, 'carNumber은', carNumber, 'carImage는', carImage)
   }
-  console.log(currentUser.email);
 
-  const [carName, setCarName] = useState('');
-  const [carNumber, setCarNumber] = useState('');
-  const [carImage, setCarImage] = useState('');
-  const [carNameError, setCarNameError] = useState(false);
-  const [carNumberError, setCarNumberError] = useState(false);
+  const [oilChangePrice, setOilChangePrice] = useState('');
+  const [oilChangeDate, setOilChangeDate] = useState();
+  const [oilChangeName, setOilChangeName] = useState('');
+  const [oilChangeMemo, setOilChangeMemo] = useState('');
+
+  const [oilChangePriceError, setOilChangePriceError] = useState(false);
+  const [oilChangeDateError, setOilChangeDateError] = useState(false);
 
   // 차량 정보 불러오기 기능 구현
 
-  const [readData, setReadData] = useState([]);
-  const Data = async () => {
-    const q = query(collection(db, 'carManagement'));
-    const querySnapshot = await getDocs(q, 'snapshot');
-    const data = querySnapshot.docs.map((doc) => ({
-      ...doc.data(),
-      id: doc.id,
-    }));
-    setReadData(data)
-  };
-  useEffect(() => {
-    Data();
-  }, []);
-
-  useEffect(() => {
-
-    if (idData.id === undefined) {
-      console.log('데이터가 없습니다.')
-      setAddCar(false);
-    } else {
-      console.log('데이터가 있습니다.')
-      setAddCar(true);
-    }
-
-  }, [readData])
-
-  // console.log('readData', readData)
-
-
-  // id 값에 맞는 데이터만 idData에 저장하기
-  let idData = {}
-  for (let i = 0; i < readData.length; i++) {
-    if (readData[i].userEmail === currentUser.email) {
-      idData = readData[i];
-    }
-  }
-  console.log('idData',idData)
-
-  const [value, setValue] = useState(null);
-
   return   <><>
     <IconButton
-      sx={{
-        width: '100%',
-        height: 120,
-        backgroundColor: '#fafafa',
-        border: 1,
-        borderRadius: 3,
-        borderColor: 'grey.500',
-        boxShadow: 0,
-        '&:hover': {
-          backgroundColor: '#eeeeee',
-          opacity: [0.9, 0.8, 0.7],
-          boxShadow: 1,
-        },
+      sx={{width: '100%', height: 120, backgroundColor: '#fafafa', border: 1, borderRadius: 3, borderColor: 'grey.500', boxShadow: 0,
+        '&:hover': { backgroundColor: '#eeeeee', opacity: [0.9, 0.8, 0.7], boxShadow: 1, },
       }}
       onClick={handleClickOpen}
     >
-
-        <AddCircleOutlineIcon color="#fafafa" fontSize="large" />
-        <p>신규 관리 내역 추가</p>
+      <AddCircleOutlineIcon color="#fafafa" fontSize="large" />
+      <p>엔진 오일 관리 내역 추가</p>
     </IconButton>
 
-    차량 정보 입력
     <Dialog open={open} onClose={handleClose}>
       <form noValidate autoComplete="off" onSubmit={handleSubmit}>
         <DialogTitle>엔진 오일 관리 내역 등록</DialogTitle>
         <DialogContent>
-
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DatePicker
-        label="Basic example"
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-        renderInput={(params) => <TextField {...params} />}
-      />
-    </LocalizationProvider>
-
           <TextField
-            value={engineOilChangeDay}
-            onChange={(e) => setCarName(e.target.value)}
-            error={carNameError}
+            value={oilChangeDate}
+            error={oilChangeDateError}
+            id="oilChangeDate"
+            label="엔진 오일 교체 날짜"
+            type="date"
+            margin="dense"
+            // defaultValue="todate"
+            sx={{ width: 220 }}
+            InputLabelProps={{ shrink: true }}
+          />
+        </DialogContent>
+
+        <DialogContent>
+          <TextField
+            value={oilChangePrice}
+            onChange={(e) => setOilChangePrice(e.target.value)}
+            error={oilChangeDateError}
             required
             autoFocus
             margin="dense"
-            id="engineOilChangeDay"
-            label="교체 날짜"
+            id="oilChangePrice"
+            label="엔진 오일 교체 가격"
             type="string"
             fullWidth
             variant="standard" />
         </DialogContent>
 
         <DialogContent>
-
           <TextField
-            value={carNumber}
-            onChange={(e) => setCarNumber(e.target.value)}
-            error={carNumberError}
-            required
+            value={oilChangeName}
+            onChange={(e) => setOilChangeName(e.target.value)}
             autoFocus
             margin="dense"
-            id="CarNum"
-            label="교체 가격"
+            id="oilChangeName"
+            label="엔진 오일 상품 명"
             type="string"
             fullWidth
-            variant="standard" />
+            variant="standard" 
+          />
         </DialogContent>
 
         <DialogContent>
-
           <TextField
-            value={carImage}
-            onChange={(e) => setCarImage(e.target.value)}
+            value={oilChangeMemo}
+            onChange={(e) => setOilChangeMemo(e.target.value)}
             autoFocus
             margin="dense"
-            id="CarNum"
-            label="상품 명"
+            id="oilChangeMemo"
+            label="기타 사항"
             type="string"
             fullWidth
-            variant="standard" />
+            variant="standard"
+          />
         </DialogContent>
 
         <DialogActions>
